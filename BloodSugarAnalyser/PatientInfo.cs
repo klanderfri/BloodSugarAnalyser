47f3d128ca2ab327363ad711aa5ed3adf75fb431
klanderfri <registrering+github@bjornlarsson.se>
2019-07-12 16:44:45 +0200

    Commented class.

--- BloodSugarAnalyser/PatientInfo.cs
+++ BloodSugarAnalyser/PatientInfo.cs
@@ -2,13 +2,35 @@
 
 namespace BloodSugarAnalyser
 {
+    /// <summary>
+    /// Class for object holding information about the patient the log belongs to.
+    /// </summary>
     class PatientInfo
     {
+        /// <summary>
+        /// The first name the patient.
+        /// </summary>
         public string FirstName { get; private set; }
+
+        /// <summary>
+        /// The surname the patient.
+        /// </summary>
         public string Surname { get; private set; }
+
+        /// <summary>
+        /// The ID the patient.
+        /// </summary>
         public int PatientID { get; private set; }
+
+        /// <summary>
+        /// The date of birth of the patient.
+        /// </summary>
         public DateTime DateOfBirth { get; private set; }
         
+        /// <summary>
+        /// Stores the patient info from a log line.
+        /// </summary>
+        /// <param name="logLine">The log line to store patient info from.</param>
         public void StoreInfo(ClarityLogLine logLine)
         {
             if (logLine.EventType == "FirstName")
