4b6bf3fe65e4a951c0ee47061d31220540227be1
klanderfri <registrering+github@bjornlarsson.se>
2019-07-12 17:00:05 +0200

    Commented class.

--- BloodSugarAnalyser/LogLine.cs
+++ BloodSugarAnalyser/LogLine.cs
@@ -13,35 +13,75 @@ namespace BloodSugarAnalyser
         /// The index of the log line.
         /// </summary>
         public int Index { get; private set; }
+
         /// <summary>
-        /// The time of the log.
+        /// The time when the log line was created.
         /// </summary>
         public DateTime? Timestamp { get; private set; }
+
+        /// <summary>
+        /// The type of event that caused the creation of the log line.
+        /// </summary>
         public string EventType { get; private set; }
+
+        /// <summary>
+        /// The subtype of event that caused the creation of the log line (for example, the type of alarm).
+        /// </summary>
         public string EventSubtype { get; private set; }
+
+        /// <summary>
+        /// Information about the patient.
+        /// </summary>
         public string PatientInfo { get; private set; }
+
+        /// <summary>
+        /// Information about the source device.
+        /// </summary>
         public string DeviceInfo { get; private set; }
+
+        /// <summary>
+        /// The ID of the source device.
+        /// </summary>
         public string SourceDeviceID { get; private set; }
+
         /// <summary>
         /// The blood sugar (mmol/L).
         /// </summary>
         public decimal? GlucoseValue { get; private set; }
+
         /// <summary>
         /// The amount of insulin taken (u).
         /// </summary>
         public decimal? InsulinValue { get; private set; }
+
         /// <summary>
         /// The amount of carbs eaten (grams).
         /// </summary>
         public int? CarbValue { get; private set; }
+
+        /// <summary>
+        /// The time duration (for an alarm, for example).
+        /// </summary>
         public TimeSpan? Duration { get; private set; }
+
         /// <summary>
         /// How fast the blood sugar is changing (mmol/L/min).
         /// </summary>
         public decimal? GlucoseRateOfChange { get; private set; }
+
+        /// <summary>
+        /// The internal timestamp of the transmitter.
+        /// </summary>
         public long? TransmitterTime { get; private set; }
+
+        /// <summary>
+        /// The ID of the transmitter.
+        /// </summary>
         public string TransmitterID { get; private set; }
 
+        /// <summary>
+        /// Tells if the log line holds a blood sugar value.
+        /// </summary>
         public bool IsBloodSugarLog
         {
             get
@@ -51,10 +91,14 @@ namespace BloodSugarAnalyser
             }
         }
 
-        public LogLine(string logLine)
+        /// <summary>
+        /// Creates an object holding the data of a log line.
+        /// </summary>
+        /// <param name="rawLogLine">The log line as a raw string (comma separated).</param>
+        public LogLine(string rawLogLine)
         {
-            logLine += ",";
-            var values = logLine.Split(',');
+            rawLogLine += ",";
+            var values = rawLogLine.Split(',');
 
             Index = Convert.ToInt32(values[0]);
             Timestamp = values[1] == "" ? (DateTime?)null : Convert.ToDateTime(values[1]);
@@ -74,6 +118,9 @@ namespace BloodSugarAnalyser
             checkIntegrity();
         }
 
+        /// <summary>
+        /// Checks that the available data corresponds with the type of log line.
+        /// </summary>
         private void checkIntegrity()
         {
             if (IsBloodSugarLog && GlucoseValue == null)
